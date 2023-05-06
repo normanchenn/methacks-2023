f2a9ff8288e2a5cdfd8551b83542825b106e28a3
Peter Wang <40186116+PeteMango@users.noreply.github.com>
2023-05-06 14:16:11 -0400

    updated index js

--- server/index.js
+++ server/index.js
@@ -4,8 +4,9 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
-const cohere = require('cohere-ai');
-// const openAI = require("openai");
+const cohere = require("cohere-ai");
+const https = require("https");
+const axios = require("axios");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -62,17 +63,13 @@ const newCity = new City({
 //         console.error(`Error saving ${newCity.name} to the database: ${error}`);
 //     });
 
-app.get("/api/cities/:name", async (req, res) => {
+app.get("/api/mongodb/findCity/:name", async (req, res) => {
   const { name } = req.params;
-
   try {
     const city = await City.findOne({ name });
-
     if (!city) {
       return res.status(404).json({ message: "City not found" });
-      // return res.status(404).json();
     }
-
     return res.json(city);
   } catch (error) {
     console.error(error);
@@ -87,7 +84,7 @@ app.get("/", (req, res) => {
 app.get("/api/cohere/citySummary/:city", async (req, res) => {
   try {
     cohere.init(process.env.COHERE_API_KEY);
-    const {city} = req.params;
+    const { city } = req.params;
     const response = await cohere.generate({
       prompt: `Give me a summary of ${city}`,
       model: "ca5f4bac-9c09-4770-bf7a-d043739e82a7-ft",
@@ -121,7 +118,7 @@ app.get("/api/cohere/hobbies/:interest", async (req, res) => {
 app.get("/api/cohere/weatherPrediction", async (req, res) => {
   try {
     cohere.init(process.env.COHERE_API_KEY);
-    const {activity} = req.params;
+    const { activity } = req.params;
     const activityArray = [activity];
     const response = await cohere.classify({
       model: "09119595-f5d0-42b8-9ea5-02905414db32-ft",
@@ -134,6 +131,93 @@ app.get("/api/cohere/weatherPrediction", async (req, res) => {
     res.status(400).json({ error: "An error occurred while classifying data" });
   }
 });
+
+app.get(
+  "/api/amadeus/flightPlanner/:origin/:destination/:date",
+  async (req, res) => {
+    const { origin } = req.params;
+    const { destination } = req.params;
+    const { date } = req.params;
+    const clientId = process.env.AMADEUS_API_KEY;
+    const clientSecret = process.env.AMADEUS_API_SECRET;
+
+    async function getAccessToken(apiKey, apiSecret) {
+      const baseUrl = "https://test.api.amadeus.com/v1";
+      const tokenEndpoint = "/security/oauth2/token";
+
+      try {
+        const response = await axios.post(
+          `${baseUrl}${tokenEndpoint}`,
+          `grant_type=client_credentials&client_id=${apiKey}&client_secret=${apiSecret}`,
+          {
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+          }
+        );
+
+        return response.data.access_token;
+      } catch (error) {
+        console.error(error.response.data);
+        throw new Error("Failed to retrieve access token");
+      }
+    }
+
+    async function searchFlightOffers() {
+      const baseUrl = "https://test.api.amadeus.com/v2";
+
+      try {
+        const accessToken = await getAccessToken(clientId, clientSecret);
+        const headers = {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        };
+        const params = {
+          originLocationCode: origin,
+          destinationLocationCode: destination,
+          departureDate: date,
+          adults: 2,
+        };
+        const response = await axios.get(`${baseUrl}/shopping/flight-offers`, {
+          headers,
+          params,
+        });
+
+        const jsonData = response.data;
+        const filteredFlights = jsonData.data
+          .filter((flight) => flight.price && flight.price.grandTotal)
+          .sort(
+            (a, b) =>
+              parseFloat(a.price.grandTotal) - parseFloat(b.price.grandTotal)
+          );
+
+        // Get the three cheapest flights
+        const cheapestFlights = filteredFlights.slice(0, 3);
+
+        // Print information about the cheapest flights
+        cheapestFlights.forEach((flight, index) => {
+          console.log(`Flight ${index + 1}:`);
+          for (const key in flight) {
+            console.log(`${key}:`, flight[key]);
+          }
+          console.log("---------------------------------------");
+        });
+
+        res.json(JSON.stringify(response.data));
+      } catch (error) {
+        console.error(error.response?.data || error.message);
+        res.status(500).json({ error: "Failed to retrieve flight offers" });
+      }
+    }
+
+    try {
+      await searchFlightOffers();
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "An unexpected error occurred" });
+    }
+  }
+);
 
 app.get("/", (req, res) => {
   res.send("Hello world, from methacks");
