d3956e150fb94e579f03f8bb613c80f1067abf9a
Norman Chen <113641259+normanchenn@users.noreply.github.com>
2023-05-07 01:46:14 -0400

    added more styling

--- newApp/react-app/src/components/Inputs.jsx
+++ newApp/react-app/src/components/Inputs.jsx
@@ -1,7 +1,9 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom';
 
 export const Inputs = () => {
     const [name, setName] = useState("");
+    const [display, setDisplay] = useState(false);
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log(`Name: ${name}`);
@@ -15,14 +17,25 @@ export const Inputs = () => {
                 console.log(error);
             })
     }  
+    const handleChange = (event) => {
+        setName(event.target.value);
+    }
+    useEffect(() => {
+        if (name.length > 0){
+            setDisplay(true);
+        }
+        else {
+            setDisplay(false);
+        }
+      }, [name]);
   return (
-    <div>
-        <h1>City Summary</h1>
-        <form onSubmit={handleSubmit}>
-            <label>
-                Enter City Name: <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-            </label>
-            <button type="submit">Enter</button>
+
+    <div className="bg-gradient-to-b from-orange-100 to-cyan-800 h-screen flex flex-col">
+        <div className=" h-20 flex items-center justify-center m-24 rounded-full opacity-50 bg-slate-400"><p className="text-4xl text-white">Where's your destination?</p></div>
+            {/* <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Enter text" /> */}
+        <form className="flex items-center justify-center px-60" onSubmit={handleSubmit}>
+            <input type="text" onChange={handleChange} className="border-b border-gray-500 outline-none bg-transparent w-full text-6xl p-4 text-center text-white" />
+            {display && <Link to="/stats" ><button type="submit" className="text-white border border-black bg-transparent p-2">Enter</button></Link>}
         </form>
     </div>
   )
--- newApp/react-app/src/components/Interests.jsx
+++ newApp/react-app/src/components/Interests.jsx
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react'
-
+import { Link } from 'react-router-dom';
 
 export const Interests = () => {
     const [interests, setInterests] = useState([]);
+    const [isDone, setIsDone] = useState(false);
     const addInterest = (event) => {
         event.preventDefault();
         const newInterest = event.target.interest.value;
@@ -15,24 +16,41 @@ export const Interests = () => {
     };
     useEffect(() => {
         console.log(interests);
+        if (interests.length > 0){
+            setIsDone(true);
+        }
+        else {
+            setIsDone(false);
+        }
       }, [interests]);
   return (
-    <div className="flex flex-row h-screen bg-gradient-to-b from-cyan-800 to-gray-100 text-white">
+    <div className="flex flex-row h-screen bg-gradient-to-b from-cyan-800 to-orange-100 text-white">
         <div className="flex flex-col w-1/2">
             <div className="h-1/2 text-8xl p-12">Enter Keywords</div>
             <div className="h-1/2 text-6xl p-12">Tell us what you're interested in!</div>
         </div>
         <div className="flex-grow bg-red-500 rounded-3xl m-24 bg-stone-300 justify-center text-center">
-            <form onSubmit={addInterest} className="rounded-full w-80 h-10 text-black px-4 m-4 bg-white flex flex-row">
-                <input type="text" placeholder="Search" name="interest" className="basis-10/12"/>
+            <form onSubmit={addInterest} className="rounded-full w-95 h-10 text-black px-4 m-4 bg-white flex flex-row">
+                <input type="text" placeholder="Enter Hobbies" name="interest" className="basis-10/12"/>
                 <button type="submit" className="px-2 text-gray-500">Add</button>
             </form>
-            <ul>
-            {interests.map((interest, index) => (
-                <li key={index}>{interest}</li>
-            ))}
-        </ul>
+            <div className="text-black grid grid-cols-2">
+                {interests.map((interest) => (
+                    <div key={interest} className="w-full p-6">
+                        <Block word={interest} className=""/>
+                    </div>
+                ))}
+            </div>
+            {isDone && <Link to="/inputs" className="flex justify-center"><div className="bg-white text-black rounded-lg h-20 w-1/2 flex items-center"><p className="w-full">Done?</p></div></Link>}
         </div>
     </div>
   )
+}
+
+const Block = ({ word }) => {
+    return (
+        <div className="bg-white rounded-lg h-20 flex items-center">
+            <p className="w-full">{word}</p>
+        </div>
+    )
 }
